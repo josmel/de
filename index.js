385c394a3a8045eb9785071e8251759134e7cc6e
Josmel Yupanqui <josmelnyh89@gmail.com>
2020-12-05 20:08:09 -0500

    feat(release-1.1): add change in file index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 const port = 3001
 
 app.get('/',(req,res)=> {
-res.send('hola mundo')
+   res.send('hola mundo')
 })
 
 
 app.listen(port,()=>{
-console.log(`listen ip port ${port}`)
+   console.log(`listen ip port ${port}`)
 })
